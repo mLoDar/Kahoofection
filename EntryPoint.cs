761a1414b93e191c4bc054663851aab03376cf16
mLoDar <mlodarcoding@gmail.com>
2025-02-04 17:50:55 +0100

    Structured applications entry point Added some comments to describe what should happen in the entry point.

---
+++ EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+
+
+
+
+namespace Kahoofection
+{
+    internal class EntryPoint
+    {
+        private const string _currentSection = "EntryPoint";
+
+
+
+        static async Task Main()
+        {
+            // TODO: Enable ANSI Support
+            
+            // TODO: Create needed folders/files
+
+
+
+            Console.Title = "Kahoofection";
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+
+            // TODO: Redirect to MainMenu
+            
+
+
+            Environment.Exit(0);
+        }
+    }
+}
