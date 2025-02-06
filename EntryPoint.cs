536b1f2471892d266b34e0de8e96e1e46040b81c
mLoDar <mlodarcoding@gmail.com>
2025-02-06 17:18:03 +0100

    Finished entry point When starting the application, ANSI support will be enabled and a welcome message will be shown. Additionally the application starts the main menu which is empty right now.

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -13,7 +13,6 @@ namespace Kahoofection
     {
         private const string _currentSection = "EntryPoint";
 
-        private static readonly ApplicationSettings.Paths _appPaths = new();
         private static readonly ApplicationSettings.Runtime _appRuntime = new();
 
 
@@ -30,29 +29,91 @@ namespace Kahoofection
 
 
             ActivityLogger.Log(_currentSection, "Trying to enable support for ANSI escape sequence.");
+            (bool ansiSupportEnabled, Exception occuredError) = ConsoleHelper.EnableAnsiSupport();
 
-            // TODO: Enable ANSI Support
+            if (ansiSupportEnabled == false)
+            {
+                ActivityLogger.Log(_currentSection, "[ERROR] Failed to enable ANSI support.");
+                ActivityLogger.Log(_currentSection, occuredError.Message, true);
 
+                Console.SetCursorPosition(0, 4);
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("             WARNING\r\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("             Failed to enable support for ANSI escape sequences.");
+                Console.WriteLine("             This will have side effects on the coloring within the console.\r\n");
+                Console.WriteLine("             Please read the manual on how to fix this error!");
 
-
-            ActivityLogger.Log(_currentSection, "Searching for all required folders/files.");
-
-            // TODO: Create needed folders/files
+                Thread.Sleep(5000);
+            }
+            else
+            {
+                ActivityLogger.Log(_currentSection, "Successfully enabled ANSI support!");
+            }
 
 
 
             Console.Title = "Kahoofection";
+            Console.CursorVisible = false;
             Console.OutputEncoding = Encoding.UTF8;
 
 
 
+            ActivityLogger.Log(_currentSection, "Displaying welcome message.");
+
+            await DisplayWelcomeMessage();
+
+            ActivityLogger.Log(_currentSection, "Welcome message was displayed, redirecting to the main menu.");
+
             await MainMenu.Start();
-
-
 
             ActivityLogger.Log(_currentSection, "Shutting down Kahoofection.");
 
+
+
             Environment.Exit(0);
+        }
+
+        private static async Task DisplayWelcomeMessage()
+        {
+            ConsoleHelper.ResetConsole();
+
+
+
+            string welcomeMessage = "Hello stranger! Welcome to:";
+
+            string[] kahoofectionHeader =
+            [
+                @"              _  __ __  _  _  __   __  ___ ___ ________ _  __  __  _ ",
+                @"             | |/ //  \| || |/__\ /__\| __| __/ _/_   _| |/__\|  \| |",
+                @"             |   <| /\ | >< | \/ | \/ | _|| _| \__ | | | | \/ | | ' |",
+                @"             |_|\_\_||_|_||_|\__/ \__/|_| |___\__/ |_| |_|\__/|_|\__|",
+            ];
+
+
+
+            Console.Write("             ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (char c in welcomeMessage)
+            {
+                Console.Write(c);
+                await Task.Delay(10);
+            }
+
+            await Task.Delay(500);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+             
+            foreach (string line in kahoofectionHeader)
+            {
+                await Task.Delay(100);
+                Console.WriteLine(line);
+            }
+
+            await Task.Delay(1500);
         }
     }
 }
---
+++ Scripts/ConsoleHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+
+
+
+#pragma warning disable SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+
+
+
+
+
+namespace Kahoofection.Scripts
+{
+    internal class ConsoleHelper
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(nint hConsoleHandle, int mode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(nint hConsoleHandle, out int mode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern nint GetStdHandle(int handle);
+
+
+
+        internal static (bool ansiSupportEnabled, Exception occuredError) EnableAnsiSupport()
+        {
+            try
+            {
+                nint consoleHandle = GetStdHandle(-11);
+
+                if (GetConsoleMode(consoleHandle, out int currentConsoleMode))
+                {
+                    SetConsoleMode(consoleHandle, currentConsoleMode | 0x0004);
+                }
+            }
+            catch (Exception exception)
+            {
+                return (false, exception);
+            }
+
+            return (true, new Exception());
+        }
+
+        internal static void ResetConsole()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 4);
+        }
+    }
+}
