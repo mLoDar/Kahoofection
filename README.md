47c8c1e5ea17e37c7a4ebadb4fd84f4b237ec94d
mLoDar <mlodarcoding@gmail.com>
2025-02-08 02:13:15 +0100

    Added helper methods The newly added methods will be used later on, for interacting with the console.

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Kahoofection is a bot, which is powered by Selenium and used for a variety of ta
 ## External services used
 - The website [patorjk.com](https://patorjk.com/software/taag/) was used for creating headers within the application.
 - The website [lingojam.com](https://lingojam.com/ItalicTextGenerator) was used for creating text in different fonts.
+- For drawing most of the menu [Wikipedia](https://wikipedia.org/) did a great job, on serving as a documentation on [box-drawing characters](https://en.wikipedia.org/wiki/Box-drawing_characters).
--- Scripts/ConsoleHelper.cs
+++ Scripts/ConsoleHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Text;
+using System.Runtime.InteropServices;
 
 
 
@@ -42,10 +43,90 @@ namespace Kahoofection.Scripts
             return (true, new Exception());
         }
 
+        internal static async Task<(bool escapeKeyPressed, string lineContent)> ReadLine()
+        {
+            StringBuilder sb = new();
+
+            int cursorStartPositionTop = Console.CursorTop;
+
+
+
+            while (true)
+            {
+                await Task.Delay(10);
+
+                if (Console.KeyAvailable == false)
+                {
+                    continue;
+                }
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return (true, string.Empty);
+
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return (false, sb.ToString());
+
+                    case ConsoleKey.Backspace:
+                        if (sb.Length > 0)
+                        {
+                            sb.Remove(sb.Length - 1, 1);
+
+                            int cursorLeft = Console.CursorLeft;
+                            int cursorTop = Console.CursorTop;
+
+                            if (cursorLeft == 0 && cursorTop > cursorStartPositionTop)
+                            {
+                                Console.SetCursorPosition(Console.BufferWidth - 1, cursorTop - 1);
+                                Console.Write(" ");
+                                Console.SetCursorPosition(Console.BufferWidth - 1, cursorTop - 1);
+                                break;
+                            }
+                            
+                            Console.Write("\b \b");
+                        }
+                        break;
+
+                    default:
+                        if (char.IsControl(pressedKey.KeyChar) == false && sb.Length < 4094)
+                        {
+                            sb.Append(pressedKey.KeyChar);
+                            Console.Write(pressedKey.KeyChar);
+                        }
+                        break;
+                }
+            }
+        }
+
         internal static void ResetConsole()
         {
             Console.Clear();
             Console.SetCursorPosition(0, 4);
+        }
+
+        internal static async Task DisplayInformation(string title, string description, ConsoleColor titleColor, int durationInSeconds = 3)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("             ┌───");
+            Console.Write("             │ ");
+            Console.ForegroundColor = titleColor;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("             ├───────────────");
+            Console.WriteLine("             │ {0}", description);
+            Console.WriteLine("             └───");
+
+            if (durationInSeconds <= 0 || durationInSeconds > 10)
+            {
+                await Task.Delay(5000);
+                return;
+            }
+
+            await Task.Delay(durationInSeconds * 1000);
         }
     }
 }
