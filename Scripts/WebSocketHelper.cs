467dd62a3a45d49e86b844a2338e814feb6ad84f
mLoDar <mlodarcoding@gmail.com>
2025-02-16 21:44:54 +0100

    Added new helper class This helper will hold relevant methods, related to interacting with a websocket. For now it only contains methods to send/receive messages.

---
+++ Scripts/WebSocketHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Net.WebSockets;
+
+using Newtonsoft.Json;
+
+
+
+
+
+namespace Kahoofection.Scripts
+{
+    internal class WebSocketHelper
+    {
+        internal static async Task<(bool messageSent, Exception? occuredError)> SendMessageAsync(ClientWebSocket webSocket, object message, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(webSocket);
+
+                if (webSocket.State != WebSocketState.Open)
+                {
+                    throw new Exception($"WebSocketState is not open, received as '({webSocket.State})'");
+                }
+
+                string jsonMessage = JsonConvert.SerializeObject(message, Formatting.None);
+                byte[] buffer = Encoding.UTF8.GetBytes(jsonMessage);
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, cancellationToken);
+
+                return (false, null);
+            }
+            catch (Exception exception)
+            {
+                return (false, exception);
+            }
+        }
+
+        internal static async Task<(bool receivedMessage, string messageContent, Exception? occuredError)> ReceiveMessageAsync(ClientWebSocket webSocket, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                ArgumentNullException.ThrowIfNull(webSocket);
+
+                if (webSocket.State != WebSocketState.Open)
+                {
+                    throw new Exception($"WebSocketState is not open, received as '({webSocket.State})'");
+                }
+
+                var buffer = new byte[4096];
+
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+                string messageContent = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                return (true, messageContent, null);
+            }
+            catch (Exception exception)
+            {
+                return (false, string.Empty, exception);
+            }
+        }
+    }
+}
