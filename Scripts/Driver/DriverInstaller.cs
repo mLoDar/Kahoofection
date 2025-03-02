261b8d758a26848d119a35d91112b565cc9a92ee
mLoDar <mlodarcoding@gmail.com>
2025-03-02 22:21:36 +0100

    Implemented driver installation A method was added for the driver installer, which allows to download a fitting driver for chrome, extract the zip file and save it to the Kahoofection folder. A method for downloading a gecko driver for Firefox will be added with future commits.

--- Scripts/Driver/DriverInstaller.cs
+++ Scripts/Driver/DriverInstaller.cs
@@ -1,7 +1,100 @@
-﻿namespace Kahoofection.Scripts.Driver
+﻿using System.IO.Compression;
+
+using Kahoofection.Ressources;
+
+
+
+
+
+namespace Kahoofection.Scripts.Driver
 {
     internal class DriverInstaller
     {
+        private const string _currentSection = "DriverInstaller";
 
+        private static readonly ApplicationSettings.Urls _appUrls = new();
+        private static readonly ApplicationSettings.Paths _appPaths = new();
+
+
+
+        internal static async Task<bool> DownloadChromeDriver()
+        {
+            string subSection = "DownloadChromeDriver";
+
+            try
+            {
+                string? chromePath = DriverHelper.GetBrowserPath(SupportedBrowser.Chrome);
+
+                if (string.IsNullOrEmpty(chromePath) || !File.Exists(chromePath))
+                {
+                    throw new Exception("Chrome's file path was not found, as it might not be installed.");
+                }
+
+
+
+                string? chromeVersion = DriverHelper.GetFileVersion(chromePath);
+
+                if (string.IsNullOrEmpty(chromeVersion))
+                {
+                    throw new Exception("No valid file version of chrome was found.");
+                }
+
+
+
+                string baseUrl = _appUrls.chromeDriverDownload;;
+                string requestUrl = baseUrl.Replace("{chromeVersion}", chromeVersion);
+
+                string driversFolder = _appPaths.driversFolder;
+                string localSavePath = Path.Combine(driversFolder, $"chromedriver-{chromeVersion}.zip");
+
+
+
+                try
+                {
+                    if (File.Exists(localSavePath))
+                    {
+                        File.Delete(localSavePath);
+                    }
+
+                    if (!Directory.Exists(driversFolder))
+                    {
+                        Directory.CreateDirectory(driversFolder);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception($"Could not create needed folders or delete existing files. {exception.Message}");
+                }
+                
+
+
+                Exception? downloadError = await WebConnection.DownloadFile(requestUrl, localSavePath);
+
+                if (downloadError != null)
+                {
+                    throw downloadError;
+                }
+
+
+
+                try
+                {
+                    ZipFile.ExtractToDirectory(localSavePath, driversFolder, true);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception($"Could not extract the downloaded file into the selected folder. {exception.Message}");
+                }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                ActivityLogger.Log(_currentSection, subSection, "Failed to download a chromedriver.");
+                ActivityLogger.Log(_currentSection, subSection, $"Exception: {exception.Message}", true);
+
+                return false;
+            }
+        }
     }
 }
