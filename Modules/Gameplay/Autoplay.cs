357dd4a1dd94b51ca4b31107456694c67500ca11
mLoDar <mlodarcoding@gmail.com>
2025-03-05 00:41:19 +0100

    Added new module With this commit a blank template was added for the last module which still needs to be done. The "Autoplay" module. The actual implementation will be finished with future commits.

---
+++ Modules/Gameplay/Autoplay.cs
@@ -0,0 +1,29 @@
+﻿using Kahoofection.Scripts;
+
+
+
+
+
+namespace Kahoofection.Modules.Gameplay
+{
+    internal class Autoplay
+    {
+        private const string _currentSection = "Autoplay";
+
+
+
+        internal static async Task Start()
+        {
+            string subSection = "Main";
+
+        LabelMethodEntryPoint:
+
+            Console.Title = $"Kahoofection | {_currentSection}";
+            ActivityLogger.Log(_currentSection, subSection, $"Starting module '{_currentSection}'");
+
+
+
+            ConsoleHelper.ResetConsole();
+        }
+    }
+}
