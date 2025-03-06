mLoDar/Kahoofection

commit e619c5348ceb3722a9f6db1f51f63154557877f8

Added new helper method The new KahootHelper should provide a global way, on checking if a provided input is a valid QuizId. This helper method will be implemented project wide within the next commits.

Scripts/Kahoot/KahootHelper.cs
@@ -435,5 +435,72 @@ namespace Kahoofection.Scripts.Kahoot
             }
             return (gamePin, null);
         }
+
+        internal static async Task<(Guid convertedQuizId, string apiResponse, Exception? occurredError)> ValidQuizId(string providedQuizId)
+        {
+            string subSection = "ValidQuizId";
+
+            ActivityLogger.Log(_currentSection, subSection, "Received a request to validate a string as a QuizId.");
+            ActivityLogger.Log(_currentSection, subSection, $"Provided input: {providedQuizId}");
+
+
+
+            if (string.IsNullOrWhiteSpace(providedQuizId))
+            {
+                ActivityLogger.Log(_currentSection, subSection, "An invalid QuizId was provided (Empty string).");
+
+                return (new Guid(), string.Empty, new Exception("An invalid QuizId was provided (Empty string)."));
+            }
+
+            if (Guid.TryParse(providedQuizId, out Guid convertedQuizId) == false)
+            {
+                ActivityLogger.Log(_currentSection, subSection, "An invalid QuizId was provided (No valid guuid).");
+
+                return (new Guid(), string.Empty, new Exception("An invalid QuizId was provided (No valid guuid)."));
+            }
+
+
+
+            string requestUrl = $"{_appUrls.kahootCheckQuizId.Replace("{quizId}", providedQuizId)}";
+            string apiResponse = await WebConnection.CreateRequest(requestUrl);
+
+            if (string.IsNullOrEmpty(apiResponse))
+            {
+                ActivityLogger.Log(_currentSection, subSection, "Received an invalid response from the API, the response was empty.");
+                ActivityLogger.Log(_currentSection, subSection, $"Most likely no quiz with the QuizId '{providedQuizId}' exists.", true);
+
+                return (new Guid(), string.Empty, new Exception("Invalid QuizId was provided (API response was empty)."));
+            }
+
+
+
+            JObject? foundData;
+
+            try
+            {
+                foundData = JObject.Parse(apiResponse);
+
+                if (foundData == null)
+                {
+                    throw new Exception("The parsed quiz data is null.");
+                }
+
+                if ((foundData["error"]?.ToString() ?? "NOT_FOUND").Equals("NOT_FOUND"))
+                {
+                    throw new Exception("The provided Guid was not validated as a Kahoot quiz.");
+                }
+            }
+            catch (Exception exception)
+            {
+                ActivityLogger.Log(_currentSection, subSection, "An invalid QuizId was provided, as the application failed to convert the received API response.");
+                ActivityLogger.Log(_currentSection, subSection, $"Exception: {exception.Message}", true);
+
+                return (new Guid(), string.Empty, exception);
+            }
+
+            ActivityLogger.Log(_currentSection, subSection, "The provided input was prooven as a valid QuizId!");
+
+            return (convertedQuizId, apiResponse, null);
+        }
     }
 }

commit 58e311d03aac8b0b048670a08675c4c90a5e2ad5

Fixed important error This error was introduced with commit e619c5348ceb3722a9f6db1f51f63154557877f8. In this commit a new helper method was added to validate a string as a QuizId. Within one of the check conditions, an error was introduced, which flags a valid input as an invalid QuizId. This issue was fixed with the current commit.

Scripts/Kahoot/KahootHelper.cs
@@ -485,7 +485,7 @@ namespace Kahoofection.Scripts.Kahoot
                     throw new Exception("The parsed quiz data is null.");
                 }
 
-                if ((foundData["error"]?.ToString() ?? "NOT_FOUND").Equals("NOT_FOUND"))
+                if ((foundData["error"]?.ToString() ?? string.Empty).Equals("NOT_FOUND"))
                 {
                     throw new Exception("The provided Guid was not validated as a Kahoot quiz.");
                 }