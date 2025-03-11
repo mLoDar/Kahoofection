65f1b40d607c3ba338b1724d67d15f0cbd34a056
mLoDar <mlodarcoding@gmail.com>
2025-03-11 14:01:04 +0100

    Worked on Autoplay With this commit it is finally possible to join a game with the specified settings. The entire Autoplay will be based on a webdriver which runs headless, in order to minimize user error. In the next commits, the downloading of question data and the actual answering of questions will be implemented.

--- Modules/Gameplay/Autoplay.cs
+++ Modules/Gameplay/Autoplay.cs
@@ -1,4 +1,6 @@
-﻿using Kahoofection.Scripts;
+﻿using System.Text.RegularExpressions;
+
+using Kahoofection.Scripts;
 using Kahoofection.Ressources;
 using Kahoofection.Scripts.Driver;
 using Kahoofection.Scripts.Kahoot;
@@ -38,10 +40,12 @@ namespace Kahoofection.Modules.Gameplay
 
         private static readonly ApplicationSettings.Urls _appUrls = new();
         private static readonly ApplicationSettings.Paths _appPaths = new();
+        private static readonly ApplicationSettings.DriverPaths _appDriverPaths = new();
 
         private static string _quizIdApiResponse = string.Empty;
 
         private static IWebDriver _webDriver;
+        private static List<string> _webDriverLog = [];
 
 
 
@@ -55,7 +59,7 @@ namespace Kahoofection.Modules.Gameplay
             ActivityLogger.Log(_currentSection, subSection, $"Starting module '{_currentSection}'");
 
 
-            
+
             ConsoleHelper.ResetConsole();
 
             (bool escapeKeyPressed, GameAutoplaySettings gameAutoplaySettings) = await GetGameSettings();
@@ -68,7 +72,7 @@ namespace Kahoofection.Modules.Gameplay
 
             ActivityLogger.Log(_currentSection, subSection, $"Successfully created Autoplay settings.");
 
-            
+
 
             ConsoleHelper.ResetConsole();
 
@@ -115,11 +119,88 @@ namespace Kahoofection.Modules.Gameplay
                 return;
             }
 
-
-
             ActivityLogger.Log(_currentSection, subSection, $"Successfully launched a WebDriver, trying to join the game.");
 
-            // TODO: Create mechanics to join/play the gmae
+
+
+            ConsoleHelper.ResetConsole();
+            Console.CursorVisible = false;
+
+            UpdateWebDriverLog("\u001b[92mSuccessfully launched a WebDriver.");
+
+
+
+            int gamePin = gameAutoplaySettings.gamePin;
+
+            ActivityLogger.Log(_currentSection, subSection, $"Joining the game via pin '{gamePin}'.");
+            UpdateWebDriverLog($"Joining the game via pin '{gamePin}'.");
+
+            _webDriver.Navigate().GoToUrl($"{_appUrls.kahootJoinPin.Replace("{gamePin}", gamePin.ToString())}");
+
+            
+
+            int timeout = 0;
+
+            string[] expectedUrls =
+            [
+                _appUrls.kahootJoinEnterName,
+                _appUrls.kahootJoinNamerator
+            ];
+
+            ActivityLogger.Log(_currentSection, subSection, $"Waiting 30 seconds for the site to load.");
+
+            while (expectedUrls.Contains(_webDriver.Url) == false)
+            {
+                UpdateWebDriverLog("Waiting for the site to load.");
+
+                if (timeout >= 30)
+                {
+                    ActivityLogger.Log(_currentSection, subSection, $"Site did not load after 30 seconds, leaving module.");
+
+                    UpdateWebDriverLog("\u001b[91mConnection timed out!");
+                    UpdateWebDriverLog("\u001b[97mPlease look at the error logs to fix this issue.");
+
+                    await Task.Delay(5000);
+
+                    return;
+                }
+
+                timeout++;
+                await Task.Delay(1000);
+            }
+
+            ActivityLogger.Log(_currentSection, subSection, $"Site did successfully load.");
+            UpdateWebDriverLog("\u001b[92mSite loaded successfully!");
+
+
+
+            ActivityLogger.Log(_currentSection, subSection, $"Trying to join the game by providing a name.");
+            UpdateWebDriverLog("\u001b[97mJoining the lobby, please be patient.");
+
+            try
+            {
+                JoinLobby(gameAutoplaySettings);
+            }
+            catch (Exception exception)
+            {
+                ActivityLogger.Log(_currentSection, subSection, $"Failed to join the game.");
+                ActivityLogger.Log(_currentSection, subSection, $"Exception: {exception.Message}");
+
+                UpdateWebDriverLog("\u001b[91mFailed to join the game!");
+                UpdateWebDriverLog("\u001b[97mPlease look at the error logs to fix this issue.");
+
+                await Task.Delay(5000);
+                
+                return;
+            }
+
+            ActivityLogger.Log(_currentSection, subSection, $"Successfully joined the game.");
+            UpdateWebDriverLog("\u001b[92mSuccessfully joined the lobby!");
+
+
+
+            // TODO: Create mechanics to join/play the game
+
         }
 
         private static async Task<(bool escapeKeyPressed, GameAutoplaySettings gameAutoplaySettings)> GetGameSettings()
@@ -535,6 +616,122 @@ namespace Kahoofection.Modules.Gameplay
             );
 
             _webDriver = new ChromeDriver(service, options);
+        }
+
+        private static void UpdateWebDriverLog(string message)
+        {
+            Console.SetCursorPosition(0, 4);
+
+            string formattedEntry = $"[{DateTime.Now:HH:mm:ss}] - {message}";
+
+            _webDriverLog.Add(formattedEntry);
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"               __  _  _ _____ __  ___ _    __ __   __                                                    ");
+            Console.WriteLine(@"              /  \| || |_   _/__\| _,\ |  /  \\ `v' /                                                    ");
+            Console.WriteLine(@"             | /\ | \/ | | || \/ | v_/ |_| /\ |`. .'                                                     ");
+            Console.WriteLine(@"             |_||_|\__/  |_| \__/|_| |___|_||_| !_!                                                      ");
+            Console.WriteLine("            \u001b[94m ──────────────────────────────────────────────                                     ");
+            Console.WriteLine("                                                                                                          ");
+            Console.WriteLine("                                                                                                          ");
+            Console.WriteLine("             ┌──────────────────────────────────────────────────────────────────────────────────────────┐ ");
+
+
+
+            List<string> entriesToDisplay = [];
+
+            int maxEntryLength = 88;
+            int entriesHelper = 0;
+            int entriesDisplayedAtOnce = 10;
+
+            for (int i = _webDriverLog.Count - 1; i >= 0 && entriesHelper < entriesDisplayedAtOnce; i--)
+            {
+                string currentEntry = _webDriverLog[i];
+
+                int ansiLength = GetCombinedAnsiSequenceLength(currentEntry);
+                int actualLength = currentEntry.Length - ansiLength;
+
+                if (actualLength > maxEntryLength)
+                {
+                    currentEntry = currentEntry[..(maxEntryLength - 4)] + " ...";
+                }
+
+                currentEntry = currentEntry.PadRight(maxEntryLength + ansiLength);
+                entriesToDisplay.Add(currentEntry);
+
+                entriesHelper++;
+            }
+
+            entriesToDisplay.Reverse();
+
+            for (int i = 0; i < entriesToDisplay.Count; i++)
+            {
+                Console.WriteLine($"             \u001b[94m│ \u001b[97m{entriesToDisplay[i]} \u001b[94m│ ");
+            }
+
+
+
+            Console.WriteLine("             \u001b[94m└──────────────────────────────────────────────────────────────────────────────────────────┘ ");
+            Console.WriteLine("                                                                                                                    ");
+            Console.WriteLine("                                                                                                                    ");
+            Console.WriteLine("             \u001b[94m┌ \u001b[97mUse 'Backspace' to return                                                        ");
+            Console.WriteLine("             \u001b[94m└─────────────────────────────                                                               ");
+        }
+
+        private static int GetCombinedAnsiSequenceLength(string input)
+        {
+            int combinedLength = 0;
+            MatchCollection matches = RegexPatterns.AnsiSequence().Matches(input);
+
+            foreach (Match match in matches)
+            {
+                combinedLength += match.Length;
+            }
+
+            return combinedLength;
+        }
+
+        private static void JoinLobby(GameAutoplaySettings gameAutoplaySettings)
+        {
+            if (_webDriver.Url.Equals(_appUrls.kahootJoinNamerator) == false)
+            {
+                UpdateWebDriverLog("\u001b[97mJoining game with the specified name.");
+
+                string gameUsername = gameAutoplaySettings.gameUsername;
+
+                _webDriver.FindElement(By.Id(_appDriverPaths.inputIdNickname)).SendKeys(gameUsername + Keys.Enter);
+
+                UpdateWebDriverLog("\u001b[92mSent name and confirmed it!");
+
+                return;
+            }
+
+            UpdateWebDriverLog("\u001b[93mNamerator active, can not use custom name.");
+
+
+
+            UpdateWebDriverLog("\u001b[97mStarted to spin the wheel for the namerator.");
+
+            _webDriver.FindElement(By.XPath(_appDriverPaths.buttonXpathNameratorSpin)).Click();
+
+            while (true)
+            {
+                try
+                {
+                    _webDriver.FindElement(By.XPath(_appDriverPaths.buttonXpathNameratorConfirm)).Click();
+
+                    UpdateWebDriverLog("\u001b[92mConfirmed namerator name!");
+
+                    break;
+                }
+                catch
+                {
+                    UpdateWebDriverLog("\u001b[97mNamerator wheel is spinning.");
+                }
+                Thread.Sleep(500);
+            }
         }
     }
 }
